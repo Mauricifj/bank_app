c64f4ceba99dc551b177fb474919025cd8ed0a25
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-04-28 07:40:10 -0300

    Added contact model

---
+++ lib/models/contact.dart
@@ -0,0 +1,30 @@
+class Contact {
+  final int id;
+  final String name;
+  final int account;
+
+  Contact(
+    this.id,
+    this.name,
+    this.account,
+  );
+
+  @override
+  String toString() {
+    return 'Contact{id: $id, name: $name, account: $account}';
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'name': name, 'account_number': account};
+  }
+
+  Contact.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        account = json['accountNumber'];
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'accountNumber': account,
+      };
+}
