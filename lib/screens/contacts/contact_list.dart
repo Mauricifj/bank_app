8e6c3e182c638aad109dfa7cff5bc6be8fa677d1
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-04-28 07:41:35 -0300

    Added contact list

---
+++ lib/screens/contacts/contact_list.dart
@@ -0,0 +1,97 @@
+import 'package:bankapp/components/progress.dart';
+import 'package:bankapp/database/dao/contact_dao.dart';
+import 'package:bankapp/models/contact.dart';
+import 'package:bankapp/screens/contacts/contact_form.dart';
+import 'package:bankapp/screens/transactions/transaction_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactList extends StatelessWidget {
+  final ContactDao _dao = ContactDao();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transfer'),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: List(),
+        future: _dao.contacts(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Loading();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(
+                    contact,
+                    onClick: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TransactionForm(contact),
+                        ),
+                      );
+                    },
+                  );
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Text('Unknown error');
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+  final Function onClick;
+
+  _ContactItem(
+    this.contact, {
+    @required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () => onClick(),
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.account.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
