e0b81a6c95d61f63f2d36096f01a6b4f540a8551
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-05-01 07:26:29 -0300

    Moved timeout to webclient

--- lib/network/webclient.dart
+++ lib/network/webclient.dart
@@ -2,7 +2,9 @@ import 'package:bankapp/network/interceptors/logging_interceptor.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
-final Client client =
-    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+final Client client = HttpClientWithInterceptor.build(
+  interceptors: [LoggingInterceptor()],
+  requestTimeout: Duration(seconds: 5),
+);
 
 const String baseUrl = "http://192.168.15.15:8081/transactions";
